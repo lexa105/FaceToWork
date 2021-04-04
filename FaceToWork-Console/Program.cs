a018f6687f75f3a6fbf1c3f7d305d4b1a0694876
lexa105 <56413595+lexa105@users.noreply.github.com>
2021-04-04 09:16:19 +0200

    Add files via upload

---
+++ FaceToWork-Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+namespace FaceToWork_Console
+{
+	class Program
+	{
+		const string subskey = "<tvuj klic>";
+		const string endpoint = "<tvuj endpoint>";
+
+		static void Main(string[] args)
+		{
+			IFaceClient client = Authenticate(endpoint, subskey);
+		}
+	}
+}
